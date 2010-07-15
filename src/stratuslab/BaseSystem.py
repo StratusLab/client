8c16b06df4279ce4a2025174abf596b6f2115144
Alexandre Joseph <contact@alexandrejoseph.com>
2010-07-15 16:33:34 +0200

    Escape chars (really....)

--- src/stratuslab/BaseSystem.py
+++ src/stratuslab/BaseSystem.py
@@ -228,7 +228,7 @@ class BaseSystem(object):
         escapedChars = ['[', ']', '&', '\\', '$']
         
         for char in escapedChars:
-            str.replace(char, '\\%s' % char)
+            str = str.replace(char, '\\%s' % char)
             
         return str
         
