2ba7066e886e2d42ec194d0caf2f902c193b6a2d
Alexandre Joseph <contact@alexandrejoseph.com>
2010-08-24 14:32:47 +0200

    Change default install directory

--- src/share/context/env.sh
+++ src/share/context/env.sh
@@ -3,4 +3,4 @@
 export STRATUSLAB_ENDPOINT=http://localhost:2633/RPC2
 export STRATUSLAB_USERNAME=oneadmin
 export STRATUSLAB_PASSWORD=oneadmin
-export STRATUSLAB_LOCATION=/root/stratuslab
+export STRATUSLAB_LOCATION=/tmp/stratuslab-remote-install
