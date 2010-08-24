dff12508b2231a8448cd6f9d6e8a9f3b4a0c3f06
Alexandre Joseph <contact@alexandrejoseph.com>
2010-08-24 14:24:54 +0200

    Update stratus context patch

--- src/share/context/env.sh
+++ src/share/context/env.sh
@@ -3,4 +3,4 @@
 export STRATUSLAB_ENDPOINT=http://localhost:2633/RPC2
 export STRATUSLAB_USERNAME=oneadmin
 export STRATUSLAB_PASSWORD=oneadmin
-export STRATUSLAB_LOCATION=/srv/cloud/one
+export STRATUSLAB_LOCATION=/root/stratuslab
