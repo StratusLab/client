e81d188b56867fccbb5d0692f82d6772a6ac245d
Marc-Elian Bégin <meb@mebair.local>
2010-09-08 17:15:21 +0200

    Added OneState class to manage state, and its unit test

---
+++ test/stratuslab/cloud/OneStateTest.py
@@ -0,0 +1,33 @@
+import unittest
+
+from stratuslab.cloud.one import OneState
+
+class OneStateTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+    def testState(self):
+        
+        state = OneState(0)
+        self.assertEquals('Init', str(state))
+
+        state = OneState(0,3)
+        self.assertEquals('Init', str(state))
+
+    def testLcmState(self):
+        
+        state = OneState(3,0)
+        self.assertEquals('Lcm_Init', str(state))
+
+        state = OneState(3,16)
+        self.assertEquals('Unknown', str(state))
+
+if __name__ == "__main__":
+    unittest.main()
+        
