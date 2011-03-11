ac06417bc7f5e0be323d147c1c7d4659629964df
Konstantin Skaburskas <konstan@sixsq.com>
2011-03-11 17:05:56 +0100

    STRATUSLAB-356: multiple ssh public keys can be provided in one file. Added documentation.

--- core/src/main/python/stratuslab/Runnable.py
+++ core/src/main/python/stratuslab/Runnable.py
@@ -43,7 +43,7 @@ class Runnable(AuthnCommand):
         self.parser.usage = '''%prog [defaultOptions] image'''
 
         self.parser.add_option('-k', '--key', dest='userPublicKeyFile',
-                help='SSH public key (.pub) to log on the machine. By default STRATUSLAB_KEY', metavar='FILE',
+                help='SSH public key(s) (.pub) to log on the machine. By default STRATUSLAB_KEY. In case of multiple keys, concatenate them to the file.', metavar='FILE',
                 default=defaultOptions['userPublicKeyFile'])
 
         self.parser.add_option('-t', '--type', dest='instanceType',
