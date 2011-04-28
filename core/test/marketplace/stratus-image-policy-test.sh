756fcaaa90a41a40abaf0f04920c9b56b28a94f1
airaj <airaj@lal.in2p3.fr>
2011-04-28 14:05:17 +0200

    Policy Validate plugin

--- core/test/marketplace/stratus-image-policy-test.sh
+++ core/test/marketplace/stratus-image-policy-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-../../../cli/sysadmin/src/main/python/stratus-policy-image OpX5FrzHTbdwhHxmZosLMBXm7bo policy.cfg
+../../../cli/sysadmin/src/main/python/stratus-policy-image GOaxJFdoEXvqAm9ArJgnZ0_ky6F policy.cfg
