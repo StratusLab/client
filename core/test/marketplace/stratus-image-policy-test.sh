c0d9acd46f3e4ac2272ba82bd80423ac37ec25cc
airaj <airaj@lal.in2p3.fr>
2011-04-29 13:12:08 +0200

    Validation Plugin Test

--- core/test/marketplace/stratus-image-policy-test.sh
+++ core/test/marketplace/stratus-image-policy-test.sh
@@ -18,6 +18,9 @@
 #ID : GOaxJFdoEXvqAm9ArJgnZ0_ky6F
 #Image: http://appliances.stratuslab.eu/images/base/ttylinux-9.7-i486-base/1.2/ttylinux-9.7-i486-base-1.2.img.gz
 
+
+echo "Now changing activate to no"
+
 sed  's/yes/no/g' policy.cfg > policy.tmp ; mv policy.tmp policy.cfg
 
 #In policy.cfg change activate to no
