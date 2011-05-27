c6fc411eae383bd76e3bf8ed68e088f858717154
Charles Loomis <loomis@lal.in2p3.fr>
2011-05-27 09:37:21 +0200

    correct location of password configuration file

--- core/src/main/python/stratuslab/system/BaseSystem.py
+++ core/src/main/python/stratuslab/system/BaseSystem.py
@@ -596,7 +596,7 @@ class BaseSystem(object):
         self._configureProxyDefaultUsersUsernamePassword()
 
     def _configureProxyDefaultUsersUsernamePassword(self):
-        filename = '/etc/stratuslab/login-pswd.properties'
+        filename = '/etc/stratuslab/one-proxy/login-pswd.properties'
         search = self.oneUsername
         replace = '%(oneUsername)s=%(proxyOneadminPassword)s,cloud-access' % self.__dict__
         Util.appendOrReplaceInFile(filename, search, replace)
