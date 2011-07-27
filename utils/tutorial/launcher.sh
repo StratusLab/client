a6468af20e0975db14a762428b05dac0e0eec21d
Alexandre Joseph <me@alexandrejoseph.com>
2011-07-27 15:05:35 +0200

    Add tutorial smoke test

---
+++ utils/tutorial/launcher.sh
@@ -0,0 +1,11 @@
+export STRATUSLAB_MARKETPLACE=http://onehost-5.lal.in2p3.fr:8081/
+export STRATUSLAB_USERNAME=test
+export STRATUSLAB_PASSWORD=test1122
+export STRATUSLAB_P12_CERTIFICATE=test.p12
+export STRATUSLAB_P12_PASSWORD=XYZXYZ
+export STRATUSLAB_ENDPOINT=onehost-5.lal.in2p3.fr
+export STRATUSLAB_IMAGE='http://appliances.stratuslab.eu/images/base/ttylinux-9.7-i486-base/1.3/ttylinux-9.7-i486-base-1.3.img.gz'
+export STRATUSLAB_PDISK_ENDPOINT=onehost-5.lal.in2p3.fr
+
+python ./tutorial.py
+
