33355a9d315e3730adff3d6c377a671cdc14d7f8
Marc-Elian Begin <meb@sixsq.com>
2011-10-10 17:28:15 +0200

    Heavily refactored the class removing significant duplication.  Introduced several new smaller more specialized classes. Updated and added unit tests.

--- core/src/main/python/stratuslab/marketplace/Policy.py
+++ core/src/main/python/stratuslab/marketplace/Policy.py
@@ -23,253 +23,198 @@ import os
 import ConfigParser
 import urllib2
 from stratuslab import Defaults
-try:
-    from lxml import etree
-except ImportError:
-    try:
-        # Python 2.5
-        import xml.etree.cElementTree as etree
-    except ImportError:
-        try:
-            # Python 2.5
-            import xml.etree.ElementTree as etree
-        except ImportError:
-            try:
-                # normal cElementTree install
-                import cElementTree as etree
-            except ImportError:
-                try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-                except ImportError:
-                    raise Exception("Failed to import ElementTree from any known place")
 
-import stratuslab.Util as Util
 from stratuslab.ConfigHolder import ConfigHolder
 from stratuslab.Exceptions import ValidationException, InputException
-from stratuslab.marketplace.Downloader import Downloader
+from stratuslab.marketplace.ManifestDownloader import ManifestDownloader
+
+class Filter(object):
+    
+    def __init__(self, messages):
+        self.filterItems = []
+        self.messages = messages
+        self.successMessage = None
+        self.failedMessage = None
+
+    def _loadConfig(self, sectionName, configParameterName):
+        configFile = Policy.POLICY_CFG
+        if not os.path.exists(configFile):
+            raise InputException("Can't find policy configuration file: %s" % configFile)
+        config = ConfigParser.ConfigParser()
+        config.read(configFile)
+        try:
+            rawFilterItems = config.get(sectionName, configParameterName)
+            self.filterItems = filter(None,[item.strip() for item in rawFilterItems.split('\n')])
+        except ConfigParser.NoOptionError:
+            pass
+
+    def filter(self, metadatas):
+
+        for metadata in metadatas:
+            candidate = self._getCandidate(metadata)
+            if self._filterSingle(candidate) is False:
+                metadatas.remove(metadata)
+
+        return metadatas
+
+    def _getCandidate(self, manifestInfo):
+        pass
+    
+    def _filterSingle(self, candidate):
+        pass
+    
+
+class BlackListBaseFilter(Filter):
+
+    def _filterSingle(self, candidate):
+        if candidate not in self.filterItems:
+            self.messages.append(self.successMessage % candidate)
+            return True
+        else:
+            self.messages.append(self.failedMessage % candidate)
+            return False
+
+class WhiteListBaseFilter(Filter):
+
+    def _filterSingle(self, candidate):
+        if candidate in self.filterItems:
+            self.messages.append(self.successMessage % candidate)
+            return True
+        else:
+            self.messages.append(self.failedMessage % candidate)
+            return False
+
+class WhiteListEndorsersFilter(WhiteListBaseFilter):
+    
+    def __init__(self, messages):
+        super(WhiteListEndorsersFilter, self).__init__(messages)
+        self._loadConfig('endorsers', 'whitelistendorsers')
+        self.successMessage = 'email endorser %s is whitelisted, keeping entry'
+        self.failedMessage = 'email endorser %s is not whitelisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.endorser
+
+
+class BlackListEndorsersFilter(BlackListBaseFilter):
+    
+    def __init__(self, messages):
+        super(BlackListEndorsersFilter, self).__init__(messages)
+        self._loadConfig('endorsers', 'blacklistendorsers')
+        self.successMessage = 'email endorser %s is not blacklisted, keeping entry'
+        self.failedMessage = 'email endorser %s is blacklisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.endorser
+
+
+class WhiteListChecksumsFilter(WhiteListBaseFilter):
+    
+    def __init__(self, messages):
+        super(WhiteListChecksumsFilter, self).__init__(messages)
+        self._loadConfig('checksums', 'whitelistchecksums')
+        self.successMessage = 'SHA-1 checksum image %s is whitelisted, keeping entry'
+        self.failedMessage = 'SHA-1 checksum image %s is not whitelisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.sha1
+
+
+class BlackListChecksumsFilter(BlackListBaseFilter):
+    
+    def __init__(self, messages):
+        super(BlackListChecksumsFilter, self).__init__(messages)
+        self._loadConfig('checksums', 'blacklistchecksums')
+        self.successMessage = 'SHA-1 checksum image %s is not blacklisted, keeping entry'
+        self.failedMessage = 'SHA-1 checksum image %s is blacklisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.sha1
+
+
+class WhiteListImagesFilter(WhiteListBaseFilter):
+    
+    def __init__(self, messages):
+        super(WhiteListImagesFilter, self).__init__(messages)
+        self._loadConfig('images', 'whitelistimages')
+        self.successMessage = 'image identifier %s is whitelisted, keeping entry'
+        self.failedMessage = 'image identifier %s is not whitelisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.identifier
+
+
+class BlackListImagesFilter(BlackListBaseFilter):
+    
+    def __init__(self, messages):
+        super(BlackListImagesFilter, self).__init__(messages)
+        self._loadConfig('images', 'blacklistimages')
+        self.successMessage = 'image identifier %s is not blacklisted, keeping entry'
+        self.failedMessage = 'image identifier %s is blacklisted, removing corresponding entry'
+
+    def _getCandidate(self, manifestInfo):
+        return manifestInfo.identifier
+
 
 class Policy(object):
 
     POLICY_CFG = os.path.join(Defaults.ETC_DIR, 'policy.cfg')
 
-    def __init__(self, policyConfigFilename, configHolder = ConfigHolder()):
-        self.whiteListEndorsers = ['whiteListEndorsers']
-        self.blackListEndorsers = ['blackListEndorsers']
-        self.whiteListChecksums = ['whiteListChecksums']
-        self.blackListChecksums = ['blackListChecksums']
-        self.whiteListImages = ['whiteListImages']
-        self.blackListImages = ['blackListImages']
-        self.validateMetaData = []
-        self.messageinfo = []
-        self.intersectionList = ['whiteListImages','blackListImages','whiteListChecksums','blackListChecksums']
-        self.policyConfigFilename = policyConfigFilename
+    def __init__(self, configHolder = ConfigHolder()):
+        self.messages = []
         configHolder.assign(self)
-        self._loadConfig(self.policyConfigFilename)
+        self.filters = self._loadFilters()
 
-    def _loadConfig(self, configfile):
-        if not os.path.exists(configfile):
-            raise InputException("Can't find policy configuration file: %s" % configfile)
-        config = ConfigParser.ConfigParser()
-        config.read(configfile)
-        sections=config.sections()
-        if 'whitelistendorsers' in sections:
-            for _,j in config.items('whitelistendorsers'):
-                if j != '':
-                    self.whiteListEndorsers.append(j)
-        if 'blacklistendorsers' in sections:
-            for _,j in config.items('blacklistendorsers'):
-                if j != '':
-                    self.blackListEndorsers.append(j)
-        if 'blacklistchecksum' in sections:
-            for _,j in config.items('blacklistchecksum'):
-                if j != '':
-                    self.blackListChecksums.append(j)
-        if 'whitelistchecksum' in sections:
-            for _,j in config.items('whitelistchecksum'):
-                if j != '':
-                    self.whiteListChecksums.append(j)
-        if 'whitelistimages' in sections:
-            for _,j in config.items('whitelistimages'):
-                if j != '':
-                    self.whiteListImages.append(j)
-        if 'blacklistimages' in sections:
-            for _,j in config.items('blacklistimages'):
-                if j != '':
-                    self.blackListImages.append(j)
-        if 'validatemetadatafile' in sections:
-            for _,j in config.items('validatemetadatafile'):
-                if j != '':
-                    self.validateMetaData.append(j)
-    
+    def _loadFilters(self):
+        filters = []
+
+        whiteListEndorsersFilter = WhiteListEndorsersFilter(self.messages)
+        if whiteListEndorsersFilter.filterItems:
+            filters.append(whiteListEndorsersFilter)
+
+        blackListEndorsersFilter = BlackListEndorsersFilter(self.messages)
+        if blackListEndorsersFilter.filterItems:
+            filters.append(blackListEndorsersFilter)
+
+        whiteListChecksumsFilter = WhiteListChecksumsFilter(self.messages)
+        if whiteListChecksumsFilter.filterItems:
+            filters.append(whiteListChecksumsFilter)
+
+        blackListChecksumsFilter = BlackListChecksumsFilter(self.messages)
+        if blackListChecksumsFilter.filterItems:
+            filters.append(blackListChecksumsFilter)
+
+        whiteListImagesFilter = WhiteListImagesFilter(self.messages)
+        if whiteListImagesFilter.filterItems:
+            filters.append(whiteListImagesFilter)
+
+        blackListImagesFilter = BlackListImagesFilter(self.messages)
+        if blackListImagesFilter.filterItems:
+            filters.append(blackListImagesFilter)
+
+        return filters
+
     def check(self, identifierUri):
-        if self._isActive():
-            self._validate(identifierUri)
-        self._loadDom(self._downloadManifest(identifierUri))
 
-        metadatas = self._retrieveMetadataList()
-        metadataEntries = metadatas.findall('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF')
+        manifests = self._downloadManifests(identifierUri)
 
-        filterList = [self.whiteListImages,
-                      self.blackListImages,
-                      self.whiteListEndorsers,
-                      self.blackListEndorsers,
-                      self.whiteListChecksums,
-                      self.blackListChecksums]
+        remainingMetadataEntries = manifests
         
-        remainingMetadataEntries = metadataEntries
-        
-        for list in filterList:
-            remainingMetadataEntries = self._filter(remainingMetadataEntries, list)
-            if len(remainingMetadataEntries) == 0:
-                sys.stderr.write(self._errorMessage())
-                raise ValidationException('Policy check Failed')
+        for filter in self.filters:
+            remainingMetadataEntries = filter.filter(remainingMetadataEntries)
+
+        if len(remainingMetadataEntries) == 0:
+            sys.stderr.write(self._errorMessage())
+            raise ValidationException('Policy check Failed')
         
         return remainingMetadataEntries
 
-
-    def _downloadManifest(self, identifierUri):
-        endpoint = Util.constructEndPoint(self.endpoint, 'http', '80', 'images')
-        url = endpoint + '/' + identifierUri
-        try:
-            manifest = Util.wstring(url)
-        except urllib2.HTTPError:
-            raise InputException('Failed to find metadata entry: %s' % url)                
-        return manifest
-
-    def _loadDom(self, xml):
-        self.manifest = etree.fromstring(xml)
-
-    def _retrieveMetadataList(self):
-        identifier = self._extractIdentifier()
-        endpoint = Util.constructEndPoint(self.endpoint, 'http', '80', 'images')
-        url = endpoint + '/' + identifier
-        metadataEntries = ''
-        try:
-            metadataEntries = Util.wstring(url)
-        except urllib2.HTTPError:
-            raise InputException('Failed to find metadata entries: %s' % url)                
-
-        return etree.fromstring(metadataEntries)
-
-    def _extractIdentifier(self):
-        return self.manifest.findtext('.//{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description/{http://purl.org/dc/terms/}identifier')
-        
-    #filter: inputs: metadata list as element tree.
-    #     remove unwanted element tree from metadata list
-    #     return metadata list  
-    
-    def _filter(self, metadatas, whiteOrBlackList):
-        metadata_to_remove=[]
-        if whiteOrBlackList[0] in self.intersectionList:
-            if not self._keep(metadatas[0], whiteOrBlackList):
-                metadata_to_remove.extend(metadatas)
-        else:
-            for metadata in metadatas:
-                if not self._keep(metadata, whiteOrBlackList):
-                    metadata_to_remove.append(metadata)
-        for metadata in metadata_to_remove:
-            metadatas.remove(metadata)
-        return metadatas
-
-    # keep: inputs: element tree
-    #	retrieve email endorser and checksum value from the elemet tree
-    #	return true email endorser listed in WhiteListEndorsers and checksum value is not listed in BlackListChecksums
-
-    def _keep(self, metadata, whiteOrblackList):
-        xpathPrefix = './/{http://mp.stratuslab.eu/slreq#}%s/{http://mp.stratuslab.eu/slreq#}'
-
-        # TODO: refactor this using the ManifestInfo class
-        emailendorser = metadata.findtext(xpathPrefix % 'endorser' + 'email')
-        imageidentifier = metadata.findtext('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description/{http://purl.org/dc/terms/}identifier')
-        checksumimages = metadata.findall('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description/{http://mp.stratuslab.eu/slreq#}checksum')
-
-        if (whiteOrblackList[0] == 'whiteListImages' and len(self.whiteListImages)>1):
-            return self._whiteListImagesPlugin(imageidentifier)
-        elif (whiteOrblackList[0] == 'blackListImages' and len(self.blackListImages)>1):
-            return self._blackListImagesPlugin(imageidentifier)
-        elif (whiteOrblackList[0] == 'whiteListEndorsers' and len(self.whiteListEndorsers)>1):
-            return self._whiteListEndorsersPlugin(emailendorser)
-        elif (whiteOrblackList[0] == 'blackListEndorsers' and len(self.blackListEndorsers)>1):
-            return self._blackListEndorsersPlugin(emailendorser)
-        elif (whiteOrblackList[0] == 'whiteListChecksums' and len(self.whiteListChecksums)>1):
-            return self._whiteListChecksumsPlugin(checksumimages)
-        elif (whiteOrblackList[0] == 'blackListChecksums' and len(self.blackListChecksums)>1):
-            return self._blackListChecksumsPlugin(checksumimages)
-        else:
-            print"Warning : no policy %s defined" % whiteOrblackList[0]
-            return True      
-
-    def _whiteListEndorsersPlugin(self, emailendorser):
-        if (emailendorser in self.whiteListEndorsers):
-            print "email endorser %s is  whitelisted" %emailendorser
-            return True
-        else:
-            self.messageinfo.append("email endorser %s is not whitelisted" %emailendorser)
-            return False
-
-    def _blackListEndorsersPlugin(self, emailendorser):
-        if (emailendorser not in self.blackListEndorsers):
-            print "email endorser %s is not blacklisted" %emailendorser
-            return True
-        else:
-            self.messageinfo.append("email endorser %s is blacklisted" %emailendorser)
-            return False
-    
-
-    def _whiteListChecksumsPlugin(self, checksumimages):
-        for checksumimage in checksumimages:
-            if (checksumimage.findtext('{http://mp.stratuslab.eu/slreq#}algorithm') == 'SHA-1'):
-                checksum_sha1 = checksumimage.findtext('{http://mp.stratuslab.eu/slreq#}value')
-        if (checksum_sha1  in self.whiteListChecksums):
-            print "SHA-1 checksum image %s is whitelisted" %checksum_sha1
-            return True
-        else:
-            self.messageinfo.append("SHA-1 checksum image %s is not whitelisted" %checksum_sha1)
-            return False
-
-    def _blackListChecksumsPlugin(self, checksumimages):
-        for checksumimage in checksumimages:
-            if (checksumimage.findtext('{http://mp.stratuslab.eu/slreq#}algorithm') == 'SHA-1'):
-                checksum_sha1 = checksumimage.findtext('{http://mp.stratuslab.eu/slreq#}value')
-        if (checksum_sha1 not in self.blackListChecksums):
-            print "SHA-1 checksum image %s is not blacklisted" %checksum_sha1
-            return True
-        else:
-            self.messageinfo.append("SHA-1 checksum image %s is blacklisted" %checksum_sha1)
-            return False
-
-    def _whiteListImagesPlugin(self, imageidentifier):
-        if imageidentifier in self.whiteListImages:
-            print "image identifier %s is whitelisted" %imageidentifier
-            return True
-        else:
-            self.messageinfo.append("image identifier %s is not whitelisted" %imageidentifier)
-            return False
-
-    def _blackListImagesPlugin(self, imageidentifier):
-        if imageidentifier not in self.blackListImages:
-            print "image identifier %s is not blacklisted" %imageidentifier
-            return True
-        else:
-            self.messageinfo.append("image identifier %s is blacklisted" %imageidentifier)
-            return False
-
-    def _isActive(self):
-        return (Util.isTrueConfVal(self.validateMetaData[0]) and True) or False
-
-    def _validate(self, identifierUri):
-        configHolder = ConfigHolder()
-        configHolder.set('verboseLevel', 3)
-        downloader = Downloader(configHolder)
-        downloader.download(identifierUri)
-        
-
-
+    def _downloadManifests(self, identifier):
+        return ManifestDownloader().getManifestList(identifier)
 
     def _errorMessage(self):
-        self.messageinfo.append("Image isn't valid according to site policy\n")
-        return '\n'.join(self.messageinfo)
+        self.messages.append("Image isn't valid according to site policy\n")
+        return '\n'.join(self.messages)
 
 
 
