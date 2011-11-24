73576121bd56f28bb448b37c6f523ae146244381
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2011-11-24 22:33:47 +0100

    Using getVmState() directly on the object.

--- core/src/main/python/stratuslab/Creator.py
+++ core/src/main/python/stratuslab/Creator.py
@@ -407,7 +407,7 @@ class Creator(object):
 
         if self.vmId:
             if not self.v1:
-                if self.cloud.getVmState(self.vmId) != 'Failed':
+                if self.getVmState(self.vmId) != 'Failed':
                     self._printStep('Shutting down machine')
                     self.cloud.vmStop(self.vmId)
             else:
