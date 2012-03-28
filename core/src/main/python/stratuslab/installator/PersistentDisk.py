eb50e89bf57b9960523e580454f4ade0f54200fc
Marc-Elian Begin <meb@sixsq.com>
2012-03-29 00:02:01 +0200

    Replaced /etc/init.d with service

--- core/src/main/python/stratuslab/installator/PersistentDisk.py
+++ core/src/main/python/stratuslab/installator/PersistentDisk.py
@@ -168,7 +168,7 @@ class PersistentDisk(object):
         
     def _service(self, service, action):
         printStep("Trying to %s %s service..." % (action, service))
-        self.system._nodeShell('/etc/init.d/%s %s' % (service, action))
+        self.system._nodeShell('service %s %s' % (service, action))
         
     def _overrideConfig(self, key, value):
         self._overrideValueInFile(key, value, self.pdiskConfigFile)    
