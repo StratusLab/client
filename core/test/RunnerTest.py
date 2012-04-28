b96200f40ddfc27a6d4f860e422a74e81ee30c69
Konstantin Skaburskas <konstan@sixsq.com>
2012-04-28 11:07:20 +0200

    following "proximity principal" moved test manifest back to testing class. Performed general refractor.

--- core/test/RunnerTest.py
+++ core/test/RunnerTest.py
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
 import unittest
 
 from mock.mock import Mock
@@ -28,6 +27,70 @@ from stratuslab.marketplace.ManifestDownloader import ManifestDownloader
 
 class RunnerTest(unittest.TestCase):
     
+    MANIFEST_DISKS_BUS_IDE = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:dcterms="http://purl.org/dc/terms/" xmlns:slreq="http://mp.stratuslab.eu/slreq#"
+    xmlns:slterms="http://mp.stratuslab.eu/slterms#" xml:base="http://mp.stratuslab.eu/">
+
+    <rdf:Description rdf:about="#MMZu9WvwKIro-rtBQfDk4PsKO7_">
+
+        <dcterms:identifier>MMZu9WvwKIro-rtBQfDk4PsKO7_</dcterms:identifier>
+
+        <slterms:os>linux</slterms:os>
+        <slterms:os-arch>linux</slterms:os-arch>
+        <slterms:os-version>0.1</slterms:os-version>
+        <slreq:bytes>0</slreq:bytes>
+        <dcterms:type>machine</dcterms:type>
+        <dcterms:valid>2011-01-24T09:59:42Z</dcterms:valid>
+        <dcterms:description>test</dcterms:description>
+        <dcterms:compression>test</dcterms:compression>
+        <slterms:version>1.0</slterms:version>
+        <slreq:endorsement rdf:parseType="Resource">
+            <dcterms:created>2011-01-24T09:59:42Z</dcterms:created>
+            <slreq:endorser rdf:parseType="Resource">
+                <slreq:email>jane.tester@example.org</slreq:email>
+                <slreq:subject>CN=Jane Tester,OU=Testing Department,O=StratusLab Project,C=EU</slreq:subject>
+                <slreq:issuer>CN=Jane Tester,OU=Testing Department,O=StratusLab Project,C=EU</slreq:issuer>
+            </slreq:endorser>
+        </slreq:endorsement>
+
+    </rdf:Description>
+</rdf:RDF>
+"""
+
+    MANIFEST_DISKS_BUS_VIRTIO = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:dcterms="http://purl.org/dc/terms/" xmlns:slreq="http://mp.stratuslab.eu/slreq#"
+    xmlns:slterms="http://mp.stratuslab.eu/slterms#" xml:base="http://mp.stratuslab.eu/">
+
+    <rdf:Description rdf:about="#MMZu9WvwKIro-rtBQfDk4PsKO7_">
+
+        <dcterms:identifier>MMZu9WvwKIro-rtBQfDk4PsKO7_</dcterms:identifier>
+
+        <slterms:disks-bus>virtio</slterms:disks-bus>
+
+        <slterms:os>linux</slterms:os>
+        <slterms:os-arch>linux</slterms:os-arch>
+        <slterms:os-version>0.1</slterms:os-version>
+        <slreq:bytes>0</slreq:bytes>
+        <dcterms:type>machine</dcterms:type>
+        <dcterms:valid>2011-01-24T09:59:42Z</dcterms:valid>
+        <dcterms:description>test</dcterms:description>
+        <dcterms:compression>test</dcterms:compression>
+        <slterms:version>1.0</slterms:version>
+        <slreq:endorsement rdf:parseType="Resource">
+            <dcterms:created>2011-01-24T09:59:42Z</dcterms:created>
+            <slreq:endorser rdf:parseType="Resource">
+                <slreq:email>jane.tester@example.org</slreq:email>
+                <slreq:subject>CN=Jane Tester,OU=Testing Department,O=StratusLab Project,C=EU</slreq:subject>
+                <slreq:issuer>CN=Jane Tester,OU=Testing Department,O=StratusLab Project,C=EU</slreq:issuer>
+            </slreq:endorser>
+        </slreq:endorsement>
+
+    </rdf:Description>
+</rdf:RDF>
+"""
+    
     def setUp(self):
         Runner._setCloudContext = Mock()
 
@@ -35,106 +98,90 @@ class RunnerTest(unittest.TestCase):
         reload(ConfigHolder)
 
     def testDisksBusTypeVirtio(self):
-
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-virtio.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_VIRTIO,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_')
         vm_params = runner._vmParamDict()
         self.failUnlessEqual('vd', vm_params['vm_disks_prefix'])
         
     def testDisksBusTypeIde(self):
-        
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-ide.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_IDE,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_')
         vm_params = runner._vmParamDict()
         self.failUnlessEqual('hd', vm_params['vm_disks_prefix'])
 
-    def testDisksBusTypeExtraDisk(self):
-        
+    def testDisksBusTypeExtraDiskIde(self):
         ch = ConfigHolder.ConfigHolder()
         ch.set('extraDiskSize', '1')
-
-        # IDE        
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-ide.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  ch)
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_IDE,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         extra_disk = Runner.EXTRA_DISK % {'extraDiskSize' : '1',
                                           'vm_disks_prefix' : 'hd'}
         self.failUnlessEqual(extra_disk, vm_params['extra_disk'])
 
-        # VIRTIO        
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-virtio.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  ch)
+    def testDisksBusTypeExtraDiskVirtio(self):
+        ch = ConfigHolder.ConfigHolder()
+        ch.set('extraDiskSize', '1')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_VIRTIO,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         extra_disk = Runner.EXTRA_DISK % {'extraDiskSize' : '1',
                                           'vm_disks_prefix' : 'vd'}
         self.failUnlessEqual(extra_disk, vm_params['extra_disk'])
         
-    def testDisksBusTypeReadonlyDisk(self):
-        configHolder = ConfigHolder.ConfigHolder()
-
+    def testDisksBusTypeReadonlyDiskIde(self):
+        ch = ConfigHolder.ConfigHolder()
         uuid = 'f25cd0dc-e56f-4eea-be0c-88d866a2c73c'
-        configHolder.set('readonlyDiskId', uuid)
-
-        # IDE        
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-ide.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  configHolder)
+        ch.set('readonlyDiskId', uuid)
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_IDE,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         readonly_disk = Runner.READONLY_DISK % {'readonlyDiskId' : uuid,
                                                 'vm_disks_prefix' : 'hd'}
         self.failUnlessEqual(readonly_disk, vm_params['readonly_disk'])
 
-        # VIRTIO        
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-virtio.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  configHolder)
+    def testDisksBusTypeReadonlyDiskVirtio(self):
+        ch = ConfigHolder.ConfigHolder()
+        uuid = 'f25cd0dc-e56f-4eea-be0c-88d866a2c73c'
+        ch.set('readonlyDiskId', uuid)
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_VIRTIO,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         readonly_disk = Runner.READONLY_DISK % {'readonlyDiskId' : uuid,
                                                 'vm_disks_prefix' : 'vd'}
         self.failUnlessEqual(readonly_disk, vm_params['readonly_disk'])
 
-    def testDisksBusTypeFromCommandLine(self):
-        configHolder = ConfigHolder.ConfigHolder()
-
-        # IDE
-        configHolder.set('vmDisksBus', 'ide')
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-virtio.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  configHolder)
+    def testDisksBusTypeFromCommandLineIde(self):
+        ch = ConfigHolder.ConfigHolder()
+        ch.set('vmDisksBus', 'ide')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_VIRTIO,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         self.failUnlessEqual('hd', vm_params['vm_disks_prefix'])
 
-        # VIRTIO
-        configHolder.set('vmDisksBus', 'virtio')
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-ide.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  configHolder)
+    def testDisksBusTypeFromCommandLineVirtio(self):
+        ch = ConfigHolder.ConfigHolder()
+        ch.set('vmDisksBus', 'virtio')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_IDE,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         self.failUnlessEqual('vd', vm_params['vm_disks_prefix'])
         
-        # SCSI
-        configHolder.set('vmDisksBus', 'scsi')
-        runner = self._getRunnerForManifestInFile('manifest-disks-bus-virtio.xml',
-                                                  'MMZu9WvwKIro-rtBQfDk4PsKO7_',
-                                                  configHolder)
+    def testDisksBusTypeFromCommandLineScsi(self):
+        ch = ConfigHolder.ConfigHolder()
+        ch.set('vmDisksBus', 'scsi')
+        runner = self._getRunnerForManifest(self.MANIFEST_DISKS_BUS_VIRTIO,
+                                            'MMZu9WvwKIro-rtBQfDk4PsKO7_', ch)
         vm_params = runner._vmParamDict()
         self.failUnlessEqual('sd', vm_params['vm_disks_prefix'])
 
-    def _getRunnerForManifestInFile(self, filename, imageid, ch=ConfigHolder.ConfigHolder()):
+    def _getRunnerForManifest(self, manifest, imageid, ch=ConfigHolder.ConfigHolder()):
+        self._mockManifestDownloader(manifest)
         ch.set('verboseLevel', 0)
-    
-        self._mockManifestDownloaderToReadFromFile(filename)
-        
-        runner = Runner(imageid, ch)
-        
-        return runner
+        return Runner(imageid, ch)
 
     @staticmethod
-    def _mockManifestDownloaderToReadFromFile(filename):
-        manifest = open(os.path.join(os.path.dirname(__file__), 'resources',
-                                     filename)).read()
+    def _mockManifestDownloader(manifest):
         mock = Mock(return_value = ManifestDownloader._parseXml(manifest))
         ManifestDownloader._download = mock
     
