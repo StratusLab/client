4df9b75d7b57f1fe446a5fce1f9d47e438186596
Konstantin Skaburskas <konstan@sixsq.com>
2012-07-05 00:48:32 +0200

    implemented constructor.

--- core/src/main/python/stratuslab/messaging/StompClient.py
+++ core/src/main/python/stratuslab/messaging/StompClient.py
@@ -1,7 +1,18 @@
 
+import stomp
+
+STOMP_PORT = '61613'
+
 class StompClient(object):
+
     def __init__(self, configHolder):
         configHolder.assign(self)
+        self.destination = self.msg_queue
+        
+        host, port = self.msg_endpoint.split(':')
+        port = port or STOMP_PORT
+
+        self.connection = stomp.Connection(host_and_ports = [(host, int(port))])
 
     def send(self, message):
         raise NotImplementedError()
