d3ca220b055ebc632871ce2e897fdf54c6dd2d07
Charles Loomis <cal@sixsq.com>
2012-12-10 09:12:08 +0100

    add utilities to support building the cloud-init context file (#32)

---
+++ core/src/main/python/stratuslab/cloudinit/Util.py
@@ -0,0 +1,108 @@
+#
+# Copyright (c) 2012, Centre National de la Recherche Scientifique (CNRS)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import base64
+from gzip import GzipFile
+from StringIO import StringIO
+
+from contextlib import closing
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+SIZE_ERROR_MSG = 'multipart size (%d) exceeds limit (%d)'
+
+'''
+The maximum number of bytes that can be passed as user data is
+limited (essentially by AWS).  Having such a limit avoids DOS
+attacks; enforce the limit here. 
+'''
+MAX_BYTES = 16384
+
+'''
+This creates a single multipart file from the given parts.  Each part
+must be a tuple containing mime-type and the raw contents.  The 
+mime-type should be the subtype ONLY.  All files are assumed to be 
+text files.
+'''
+def createMultipartString(parts):
+    multipart = MIMEMultipart()
+    for mimetype, content in parts:
+        part = MIMEText(content, mimetype)
+        multipart.attach(part)
+        
+    return multipart.as_string()
+
+'''
+This creates a single multipart file from the given parts.  Each part
+must be a tuple containing mime-type and the file to include.  The
+mime-type should be the subtype ONLY.  All files are assumed to be
+text files.
+'''
+def createMultipartStringFromFiles(parts):
+    multipart = MIMEMultipart()
+    for mimetype, file in parts:
+        with open(file, 'rb') as f:
+            part = MIMEText(f.read(), mimetype)
+        multipart.attach(part)
+
+    return multipart.as_string()
+
+'''
+This gzips the multipart content and then base64 encodes the result.
+The result is compatible with sending as a value in the context 
+key-value pairs supported by OpenNebula.  Note that this checks that
+the maximum size of the result does not exceed 16384 bytes.
+'''
+def encodeMultipart(multipart):
+    with closing(StringIO()) as buffer:
+        with closing(GzipFile('', 'wb', 9, buffer)) as f:
+            f.write(multipart)
+        gzipped_data = buffer.getvalue()
+
+    gzipped_size = len(gzipped_data)
+    if (gzipped_size > MAX_BYTES):
+        raise ValueError(SIZE_ERROR_MSG % (gzipped_size, MAX_BYTES))
+
+    return base64.b64encode(gzipped_data)
+
+'''
+Decodes the multipart content and returns it.  The input must be 
+a base64 encoded, gzipped file.  It will only decode values that 
+do not exceed the maximum size.
+'''
+def decodeMultipart(encoded_multipart):
+    gzipped_data = base64.b64decode(encoded_multipart)
+    gzipped_size = len(gzipped_data)
+    if (gzipped_size > MAX_BYTES):
+        raise ValueError(SIZE_ERROR_MSG % (gzipped_size, MAX_BYTES))
+
+    with closing(StringIO(gzipped_data)) as buffer:
+        with closing(GzipFile('', 'rb', 9, buffer)) as f:
+            return f.read()
+
+'''
+Creates an authorized_keys file from the given key file(s).  The
+result has one key per line (assuming that the input in on one
+line.
+'''
+def createAuthorizedKeysFromFiles(keyfiles):
+    with closing(StringIO()) as buffer:
+        for keyfile in keyfiles:
+            with open(keyfile, 'rb') as f:
+                key = f.read()
+                buffer.write(key.strip())
+            buffer.write("\n")
+        return buffer.getvalue()
---
+++ core/test/cloudinit/UtilTest.py
@@ -0,0 +1,131 @@
+#
+# Copyright (c) 2012, Centre National de la Recherche Scientifique (CNRS)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import base64
+import os
+import random
+import string
+import tempfile
+import unittest
+
+from email.Parser import Parser
+from contextlib import closing
+from gzip import GzipFile
+from StringIO import StringIO
+
+import stratuslab.cloudinit.Util as Util
+
+class UtilTest(unittest.TestCase):
+
+    TOO_LONG_RAW = None
+    TOO_LONG_GZIP = None
+
+    def setUp(self):
+        self.TOO_LONG_RAW = ''.join(random.choice(string.ascii_uppercase)
+                                    for x in range(2*Util.MAX_BYTES))
+        with closing(StringIO()) as buffer:
+            with closing(GzipFile('', 'wb', 9, buffer)) as f:
+                f.write(self.TOO_LONG_RAW)
+            self.TOO_LONG_GZIP = base64.b64encode(buffer.getvalue())
+
+    def tearDown(self):
+        pass
+
+    def testEncodeDecodeMultipart(self):
+        initial_text = 'some dummy data to check encoding and decoding'
+        encoded = Util.encodeMultipart(initial_text)
+        decoded = Util.decodeMultipart(encoded)
+
+        self.assertTrue(len(encoded) > 0)
+        self.assertTrue(len(decoded) > 0)
+        self.assertEquals(initial_text, decoded)
+
+    def testEncodeOversizedMultipart(self):
+        self.assertRaises(ValueError, Util.encodeMultipart, self.TOO_LONG_RAW)
+
+    def testDecodeOversizedMultipart(self):
+        self.assertRaises(ValueError, Util.decodeMultipart, self.TOO_LONG_GZIP)
+
+    def testCreateAuthorizedKeysFromFiles(self):
+        files = []
+        values = []
+        for i in range(3):
+            _, filename = tempfile.mkstemp()
+            files.append(filename)
+            with open(filename, 'wb') as f:
+                value = "value-%d" % i
+                f.write(value)
+                values.append(value)
+
+        result = Util.createAuthorizedKeysFromFiles(files)
+
+        for file in files:
+            os.remove(file)
+
+        self.assertEquals("\n".join(values)+"\n", result)
+
+    def testCreateMultipartString(self):
+        files = []
+        values = []
+        for i in range(3):
+            value = "value-%d" % i
+            values.append(value)
+            files.append(('plain', value))
+
+        result = Util.createMultipartString(files)
+        print result
+
+        with closing(StringIO(result)) as buffer:
+            parser = Parser()
+            msg = parser.parse(buffer)
+
+        for part in msg.walk():
+            if msg.is_multipart():
+                i = 0
+                for msg in part.get_payload():
+                    self.assertEquals(values[i], msg.get_payload())
+                    i = i + 1
+
+    def testCreateMultipartStringFromFiles(self):
+        files = []
+        values = []
+        for i in range(3):
+            _, filename = tempfile.mkstemp()
+            files.append(('plain', filename))
+            with open(filename, 'wb') as f:
+                value = "value-%d" % i
+                f.write(value)
+                values.append(value)
+
+        result = Util.createMultipartStringFromFiles(files)
+        print result
+
+        for mime, file in files:
+            os.remove(file)
+
+        with closing(StringIO(result)) as buffer:
+            parser = Parser()
+            msg = parser.parse(buffer)
+
+        for part in msg.walk():
+            if msg.is_multipart():
+                i = 0
+                for msg in part.get_payload():
+                    self.assertEquals(values[i], msg.get_payload())
+                    i = i + 1
+
+
+if __name__ == "__main__":
+    unittest.main()
